1113e90e37c0f68b975a34aa91384b750e29517a
MaximilianoFigueroaC <mfigueroa4@miuandes.cl>
2024-03-21 13:50:10 -0300

    lab2

---
+++ Script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    alert ("Welcome to my page, I hope you can get to know me a little better after reading it.")
+    
+    const title = document.getElementById("Title");
+    title.style.color = "White";
+    title.style.backgroundColor = "darkgreen"
+    title.style.textAlign = "center"
+
+}); 
+
